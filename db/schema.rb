d767e292393915716220750347ac2366ad97748a
Anna Dybas <a.m.dybas@gmail.com>
2020-03-13 16:23:16 -0400

    changed users columns from name to first_name, last_name, and username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_200151) do
+ActiveRecord::Schema.define(version: 2020_03_13_201812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,9 +56,11 @@ ActiveRecord::Schema.define(version: 2020_03_10_200151) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
   end
 
 end
