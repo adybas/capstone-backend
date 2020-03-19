61874d3cf690cb372d7200906c665311d0ed60e6
Anna Dybas <a.m.dybas@gmail.com>
2020-03-19 00:07:01 -0400

    added create to recipesController

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,7 @@
 class RecipesController < ApplicationController
+    def create
+        recipe = Recipe.create(vegetarian: params["vegetarian"], vegan: params["vegan"], glutenFree: params["glutenFree"], dairyFree: params["dairyFree"], weightWatcherSmartPoints: params["weightWatcherSmartPoints"], lowFodmap: params["lowFodmap"], preparationMinutes: params["preparationMinutes"], cookingMinutes: params["cookingMinutes"], sourceUrl: params["sourceUrl"], spoonacularSourceUrl: params["spoonacularSourceUrl"], spoonacularScore: params["spoonacularScore"], creditsText: params["creditsText"], sourceName: params["sourceName"], title: params["title"], db_id: params["db_id"], servings: params["servings"], readyInMinutes: params["readyInMinutes"], image: params["image"], summary: params["summary"], instructions: params["instructions"])
+        render json: recipe
+    end
 end
+
