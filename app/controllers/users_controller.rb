dc0edc54a1bbb41ef21f66684406d85b51bbf9f1
Anna Dybas <a.m.dybas@gmail.com>
2020-03-22 19:30:31 -0400

    changed user controller create to match show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,11 @@ class UsersController < ApplicationController
 
     def show
         user = User.find_by(username: params[:username])
-        render json:  { user: user, favorites: user.recipes }
+        render json: { user: user, favorites: user.recipes }
     end
 
     def create
         user = User.create(first_name: params["first_name"], last_name: params["last_name"], username: params["username"])
-        render json: user
+        render json: { user: user, favorites: [] }
     end
 end
